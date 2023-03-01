377aaa27c31513ddd03efb7c62f581050f7d45d3
belin cyril <cbdw77@gmail.com>
2023-03-01 11:24:27 +0100

    rien

--- services/temperature-service.js
+++ services/temperature-service.js
@@ -4,7 +4,6 @@ import {UNITS } from "../constant";
 function getOppositUnit(unit) {
     return unit == UNITS.celcius ? UNITS.faranheit : UNITS.celcius;
 }
-
 function  convertTemperatureTo(unit, value){
     if (unit == UNITS.celcius) {
         return(value - 32) / 1.8
@@ -12,7 +11,6 @@ function  convertTemperatureTo(unit, value){
         return  value * 1.8 + 32
     }
 }
-
 function isIceTemperature(value, unit){
     if(unit == UNITS.celcius){
         return value <=0
